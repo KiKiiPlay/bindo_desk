de13c43f4cd2f057640b442238ba0186f79aa9ed
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-10-13 23:59:29 +0200

    Code Update

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
+const randNum = function () {
+  console.log(newCells);
+};
+
 const createCells = function () {
-  const bingoDesk = document.getElementById("bingo_desk");
+  const bingoDesk = document.querySelector(".bingo_desk");
   for (let i = 0; i < 76; i++) {
-    const newCells = document.createElement("div");
-    const cellsNum = document.createElement("h4");
-    cellsNum.innerText = i + 1;
-    cellsNum.classList.add("cellsNum");
-    newCells.classList.add("cells");
-    newCells.appendChild(cellsNum);
-    bingoDesk.appendChild(newCells);
+    bingoDesk.innerHTML += `<div class="cells">${i + 1}</div>`;
+    console.log(i + 1);
   }
+};
+
+const generateRandNum = function () {
+  const random = Math.floor(Math.random() * 76);
+  const showRand = document.getElementById("randNum");
+  showRand.innerText = "Number is: " + random;
 };
 
 window.onload = function () {
   createCells();
+  const randomNum = document.getElementById("centerObj");
+  randomNum.addEventListener("click", generateRandNum);
 };
