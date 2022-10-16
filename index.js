dfc2790abf692e58ed1ded5dad890e79a94d036a
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-10-16 17:59:31 +0200

    Adding colorizing randomly chosen cells

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+const bingoDesk = document.querySelector(".bingo_desk");
+const readyListOfCells = document.getElementsByClassName("cells"); //difference betwen query and class
+const randArr = [];
+const randomButton = document.getElementById("centerObj");
+
 const createCells = function () {
-  const bingoDesk = document.querySelector(".bingo_desk");
   for (let i = 0; i < 76; i++) {
-    bingoDesk.innerHTML += `<div class="cells">${i + 1}</div>`;
-    console.log(i + 1);
+    bingoDesk.innerHTML += `<span class="cells">${i + 1}</span>`;
   }
+};
+
+const createRandArr = function () {
+  for (let i = 0; i < 76; i++) {
+    randArr.push(i + 1);
+  }
+};
+
+// TODO: Manage problem with functionality of colorizing randomly chosen cells
+const generateRandNum = function () {
+  const textRand = document.getElementById("randNum");
+  const randNumber = Math.floor(Math.random() * randArr.length) + 1;
+  textRand.innerText = randNumber;
+  index = randNumber - 1;
+
+  readyListOfCells[index].classList.add("newBackground");
+  randArr.splice(randNumber, 1)[0];
 };
 
 window.onload = function () {
   createCells();
+  createRandArr();
+  // difference onclick and eventlistener
+  randomButton.addEventListener("click", generateRandNum);
+  console.log(randArr);
 };
